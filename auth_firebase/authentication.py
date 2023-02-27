4780ae53e3356ce07fa8beccfc44f6a0fcf01430
Aashish <aashishkumar12376@gmail.com>
2023-02-27 21:48:08 +0530

    [ADDED] docstring in firebase auth.

--- auth_firebase/authentication.py
+++ auth_firebase/authentication.py
@@ -6,6 +6,8 @@ from firebase_admin import credentials, auth
 from django.conf import settings
 
 
+"""SETUP FIREBASE CREDENTIALS"""
+
 cred = credentials.Certificate({
     "type": settings.FIREBASE_ACCOUNT_TYPE,
     "project_id": settings.FIREBASE_PROJECT_ID,
@@ -22,12 +24,21 @@ cred = credentials.Certificate({
 default_app = firebase_admin.initialize_app(cred)
 
 
+"""FIREBASE AUTHENTICATION"""
 class FirebaseAuthentication(BaseAuthentication):
+    
+    """override authenticate method and write our custom
+        firebase authentication."""
     def authenticate(self, request):
+
+        """Get the authorization Token, 
+            It raise exception when no authorization Token is given"""
         auth_header = request.META.get("HTTP_AUTHORIZATION")
         if not auth_header:
             raise exceptions.NoAuthToken("No auth token provided")
 
+        """Decoding the Token
+            It rasie exception when decode failed."""
         id_token = auth_header.split(" ").pop()
         decoded_token = None
         try:
@@ -35,14 +46,17 @@ class FirebaseAuthentication(BaseAuthentication):
         except Exception:
             raise exceptions.InvalidAuthToken("Invalid auth token")
 
+        """Return Nothing"""
         if not id_token or not decoded_token:
             return None
 
+        """Get the uid of an user"""
         try:
             uid = decoded_token.get("uid")
         except Exception:
             raise exceptions.FirebaseError()
 
+        """Get or create the user"""
         user, created = User.objects.get_or_create(username=uid)
 
         return (user, None)
